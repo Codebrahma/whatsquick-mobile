8bc5bf8636cae826bb66dcc405c3ee2c889f3c70
Arjun <snarjun92@gmail.com>
2018-01-12 18:12:03 +0530

    Fix toolbar style

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { View, TextInput, Linking, Button } from 'react-native'
+import { View, TextInput, Linking, Button, StyleSheet } from 'react-native'
+import Toolbar from './toolbar';
 
 class App extends Component {
   constructor(props) {
@@ -12,20 +13,55 @@ class App extends Component {
 
   render() {
     return (
-      <View style={{ padding: 10 }}>
-        <TextInput
-          style={{ height: 40 }}
-          placeholder="Enter number with country code" keyboardType="phone-pad"
-          onChangeText={(text) => this.setState({ text })}
-        />
-        <Button
-          onPress={() => { Linking.openURL('https://api.whatsapp.com/send?phone='+this.state.text) }
-          }
-          title="Send Message"
-        />
+      <View>
+        <Toolbar toolbarStyles={ styles.toolbar }
+        toolbarTitleStyles={ styles.title }
+          title="WhatsQuick" />
+        <View style={{padding: 10}}>
+          <TextInput
+            style={{ height: 40 }}
+            placeholder="Enter number with country code" keyboardType="phone-pad"
+            onChangeText={(text) => this.setState({ text })}
+          />
+          <Button
+            onPress={() => { Linking.openURL('https://api.whatsapp.com/send?phone='+this.state.text) }
+            }
+            title="Send Message"
+          />
+        </View>
       </View>
     )
   }
 }
+
+const baseStyles = {
+	container: {
+		flex: 1,
+    backgroundColor:'#F2F2F2',
+    
+	},
+	title: {
+		color: '#579adf',
+    textAlign: 'center',
+		fontSize: 20
+	},
+	toolbar: {
+    backgroundColor: '#FFFFFF',
+    height: 50
+  },
+  tabbar: {
+    backgroundColor:'#FFFFFF',
+  },
+  selectedTabStyle: {
+		backgroundColor: '#F2F2F2',
+		borderColor: '#579adf',
+		borderTopWidth: 1
+  },
+  iconStyle: {
+    width: 25,
+    height:25
+  }
+}
+const styles = StyleSheet.create(baseStyles);
 
 export default App
---
+++ toolbar.js
@@ -0,0 +1,28 @@
+'use strict';
+
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image, Platform } from 'react-native';
+
+export default function(props) {
+	return (
+		<View style={ [ props.toolbarStyles, styles.toolbarStyles, { marginTop: (Platform.OS === 'ios') ? 20 : 0 } ] }>
+			<View style={{flex: 5}}>
+      	<Text style={ [props.toolbarTitleStyles] }>{ props.title }</Text>
+      </View>
+    </View>
+	)
+};
+
+const styles = StyleSheet.create({
+	toolbarStyles: {
+		padding: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+		justifyContent: 'center',
+		elevation: 2
+	},
+	iconStyle: {
+		width: 20,
+		height: 20
+	}
+})
